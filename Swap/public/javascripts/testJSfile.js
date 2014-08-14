de9faa5715ed78563e12465c008f015697829e9e
amitSA <send.amit@yahoo.com>
2014-08-13 22:19:46 -0700

    worked on testJSFile.js and index.jade

---
+++ Swap/public/javascripts/testJSfile.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("li").click(function () {
+        $("li").removeClass("active");
+        $(this).addClass("active");
+        var id = $(this).find("a").attr("href");
+        
+
+        
+
+        id = id.substring(1);
+        $("div[class=tab-content]").find("div").removeClass("active");
+        $("div[id="+id+"]").addClass("active");
+    });
+});
+
+/*alert(id);
+$("div[id=login]").removeClass("active");
+$("div[id=signup]").addClass("active");*/
