d589ef2f5b29e02b520e30c6fa889a46b6ce188e
amitSA <send.amit@yahoo.com>
2014-09-06 11:29:38 -0700

    modified web.config file

--- Swap/app.js
+++ Swap/app.js
@@ -10,7 +10,7 @@ var app = express();
 module.exports = app;
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
