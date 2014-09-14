b0a7c23da163e2f08f3c0b17ba7c03528e5fefbd
amitSA <send.amit@yahoo.com>
2014-09-13 22:40:37 -0700

    added userlayout.jade and modified some other view files

--- Swap/routes/user.js
+++ Swap/routes/user.js
@@ -16,7 +16,8 @@ app.get(/^\/home/, func.markLoginStatus,function (req, res, next) {
   var email = req.session["email-sess"];
   tableSvc.retrieveEntity("UserTable", email, "userinfo", function (error, result, response) {
     console.log(result);
-    res.send("<h1>Welcome " + result.name._ + "</h1>");  //IS THERE A FORMAT SPECIFIER FUNCTION FOR JAVASCRIPT
+    //res.send("<h1>Welcome " + result.name._ + "</h1>");  //IS THERE A FORMAT SPECIFIER FUNCTION FOR JAVASCRIPT
+    res.render("./prod/userhome", {name : result.name._});
   });                                                                                                                                            
 });
 
